add5f0b0390ab88838d375d7d5bf3aa25c30dd92
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 16:54:21 +0000

    Integrated friendly routes for blog posts

--- AppName/app/controllers/blogs_controller.rb
+++ AppName/app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blog = Blog.all
+    @blogs = Blog.all
   end
 
   # GET /blogs/1
@@ -64,7 +64,7 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
