e43fe47a2f761a06fdd8dc8b97c9d1d6bae6e60d
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 15:29:49 +0000

    changed some routes

--- AppName/config/routes.rb
+++ AppName/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  
-  get 'pages/about'
-  get 'pages/contact'
-  
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs
 
-  route to: 'pages#home'
+  root to: 'pages#home'
 end
