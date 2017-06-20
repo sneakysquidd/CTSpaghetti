33ae8d078b86812727f5abe5e4ee79a72210323e
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 15:17:34 +0000

    changed routes

--- AppName/config/routes.rb
+++ AppName/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios
-  resources :blogs
-  get 'pages/home'
 
+  
   get 'pages/about'
-
   get 'pages/contact'
+  
+  resources :blogs
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  route to: 'pages#home'
 end
