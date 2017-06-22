47095f34625321e3bd381169db6501c2b660f60e
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 15:31:28 +0000

    portfolio changes

--- AppName/app/controllers/portfolios_controller.rb
+++ AppName/app/controllers/portfolios_controller.rb
@@ -2,33 +2,32 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
   end
-  
+
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
-        
-        
       end
     end
   end
-  
-def edit
+
+  def edit
     @portfolio_item = Portfolio.find(params[:id])
-end
+  end
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
+
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:tititle, :subtitle, :body))
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
@@ -37,10 +36,10 @@ end
   end
 
   def show
-     @portfolio_item = Portfolio.find(params[:id])
-  end 
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
-   def destroy
+  def destroy
     # Perform the lookup
     @portfolio_item = Portfolio.find(params[:id])
 
@@ -51,6 +50,6 @@ end
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
-   end
+  end
 
 end
--- AppName/db/schema.rb
+++ AppName/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620153618) do
+ActiveRecord::Schema.define(version: 20170620163747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20170620153618) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
+    t.integer  "status"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
