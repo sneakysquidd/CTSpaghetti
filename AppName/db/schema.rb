060db25f32426e72cee9045d5c19d5557a55274f
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 16:27:26 +0000

    deleted extra apps and added more authentication

--- AppName/db/schema.rb
+++ AppName/db/schema.rb
@@ -18,10 +18,10 @@ ActiveRecord::Schema.define(version: 20170627142628) do
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.string   "slug"
-    t.integer  "status"
+    t.integer  "status",     default: 0
     t.integer  "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
     t.index ["topic_id"], name: "index_blogs_on_topic_id", using: :btree
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20170627142628) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "name"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
