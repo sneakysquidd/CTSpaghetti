5a01637071e90356b066f6574454725dd085002b
Jordan Hudgens <jordan@devcamp.com>
2017-07-19 14:00:31 +0000

    font awesome pagination and more

--- AppName/app/helpers/application_helper.rb
+++ AppName/app/helpers/application_helper.rb
@@ -17,6 +17,6 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+    ThomasViewTool::Renderer.copyright 'Gavin Thomas', 'All rights reserved'
   end
 end
