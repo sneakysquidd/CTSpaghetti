453b337e5e08b01d758df7f0610138423c4d8287
Jordan Hudgens <jordan@devcamp.com>
2017-07-27 17:32:51 +0000

    implemented twitter call on texh news page

--- AppName/config/routes.rb
+++ AppName/config/routes.rb
@@ -5,16 +5,16 @@ Rails.application.routes.draw do
   end
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  resources :blogs
-  
+
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  
+  get 'tech-news', to: 'pages#tech_news'
+
   resources :blogs do
     member do
       get :toggle_status
     end
   end
-
+  
   root to: 'pages#home'
 end
