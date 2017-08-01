083da6be5aff5f1472bfb24da8130d38ce419f15
Jordan Hudgens <jordan@devcamp.com>
2017-08-01 17:39:29 +0000

    improved source tracker for pages

--- AppName/app/helpers/blogs_helper.rb
+++ AppName/app/helpers/blogs_helper.rb
@@ -22,4 +22,9 @@ module BlogsHelper
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     markdown_to_html.render(text).html_safe
   end
+  
+  
+  def blog_status_color blog
+    'color: green;' if blog.draft?
+  end
 end
